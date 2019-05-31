e9f75147158632b35785d2214fdc37914948177f
Jortiz23 <Jortiz23@verizon.net>
2019-05-31 12:16:29 -0700

    In the process of transferring from javascript to typescript

---
+++ src/client/src/routes/home/LineChart.tsx
@@ -0,0 +1,128 @@
+import { Component, h } from 'preact';
+import './LineChart.css';
+interface Props{
+  data: object;
+  barColor: string;
+  labelColor: string;
+  svgWidth: number;
+  svgHeight:number;
+  barOffset:number;
+}
+interface State{
+
+}
+class LineChart <PropsType = JSX.HTMLAttributes, StateType = {}> extends Component<Props, State> {
+    public getMaxValue() {
+        const { data } = this.props;
+        const onlyMetrics = Object.values(data);
+        const maxValue = Math.max.apply(null, onlyMetrics);
+        return Math.round(maxValue/100)*100;
+    }
+    public getNumOfMetrics() {
+        const { data } = this.props;
+        return Object.keys(data).length
+    }
+    public getSvgX(x: number) {
+        const { svgWidth } = this.props;
+        return (x / this.getNumOfMetrics() * svgWidth);
+    }
+    public getSvgY(y:number) {
+        const { svgHeight } = this.props;
+        return svgHeight - (y / this.getMaxValue() * svgHeight);
+    }
+    public makeRects(){
+        const { data, svgHeight, barColor, barOffset } = this.props;
+        let htmlString: string = '';
+        const valueArr = Object.values(data);
+        let height:number;
+        for(let i = 0; i < valueArr.length; i++){
+          height = this.getSvgY(valueArr[i]);
+          htmlString+=`<rect
+                          x=${this.getSvgX(i+1)-barOffset}
+                          y=${height}
+                          width=${barOffset*2}
+                          height=${svgHeight-height}
+                          style="fill:${barColor}"
+                        />`
+        }
+        return (
+          <g dangerouslySetInnerHTML={{__html: htmlString}}/>
+        )
+    }
+    public makeValueLabels(){
+        const { data, svgHeight, labelColor, barOffset } = this.props;
+        let htmlString: string = '';
+        const valueArr = Object.values(data);
+        let height: number;
+        for(let i = 0; i < valueArr.length; i++){
+          height = this.getSvgY(valueArr[i]);
+          htmlString+=`<text
+                          x="${this.getSvgX(i+1)-barOffset}"
+                          y="${height-1}"
+                          style="fill:${labelColor}"
+                        >${valueArr[i]}</text>`
+        }
+        return (
+          <g dangerouslySetInnerHTML={{__html: htmlString}}/>
+        )
+    }
+    public makeXAxisLabels(){
+        const { data, svgHeight, labelColor, barOffset } = this.props;
+        let htmlString: string ='';
+        const keyArr = Object.keys(data);
+        const height: number;
+        for(let i = 0; i < keyArr.length; i++){
+          htmlString+=`<text
+                          x="${this.getSvgX(i+1)-barOffset}"
+                          y="${svgHeight+20}"
+                          style="fill:${labelColor}"
+                        >${keyArr[i]}</text>`
+        }
+        return (
+          <g dangerouslySetInnerHTML={{__html: htmlString}}/>
+        )
+    }
+
+    public makeAxis() {
+        const minX = 0
+        const maxX = this.getNumOfMetrics()+1
+        const minY = 0
+        const maxY = this.getMaxValue()
+        return (
+            <g className="lineChartAxis">
+                <line
+                    x1={this.getSvgX(minX)}
+                    y1={this.getSvgY(minY)}
+                    x2={this.getSvgX(maxX)}
+                    y2={this.getSvgY(minY)}
+                />
+                <line
+                    x1={this.getSvgX(minX)}
+                    y1={this.getSvgY(minY)}
+                    x2={this.getSvgX(minX)}
+                    y2={this.getSvgY(maxY)}
+                />
+            </g>
+        )
+    }
+    public render() {
+        const { svgHeight, svgWidth } = this.props;
+        return (
+            <svg viewBox={`0 0 ${svgWidth} ${svgHeight}`}>
+                {this.makeRects()}
+                {this.makeAxis()}
+                {this.makeValueLabels()}
+                {this.makeXAxisLabels()}
+            </svg>
+        )
+    }
+}
+LineChart.defaultProps = {
+    data: [],
+    barColor: 'blue',
+    labelColor: 'red',
+    svgHeight: 800,
+    svgWidth: 800,
+    barOffset: 30,
+}
+export default LineChart
--- src/client/src/routes/home/index.tsx
+++ src/client/src/routes/home/index.tsx
@@ -1,4 +1,5 @@
 import { Component, h } from "preact";
+import LineChart from './LineChart';
 import * as style from "./style.css";
 
 interface Props {}
@@ -8,6 +9,21 @@ export default class Home extends Component<Props> {
             <div class={style.home}>
                 <h1>Home</h1>
                 <p>This is the Home component.</p>
+                <svg className="graph" width="90%" height="1000">
+            			<LineChart data={{
+            				a: 92,
+            				b: 5,
+            				c: 56,
+            				d: 7,
+            				e: 67,
+            				f: 3,
+            				g:23,
+            				h:5,
+            				i:23,
+            				j:2,
+            				k:45
+            			}} barColor="abcdef"/>
+            		</svg>
             </div>
         );
     }
