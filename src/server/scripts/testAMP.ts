b89f829fca7ab46ca4f75087d4772eae763d5aab
Anthony Odum <anthonyodum@google.com>
2019-06-03 09:38:51 -0700

    changed variables to const

--- src/server/scripts/testAMP.ts
+++ src/server/scripts/testAMP.ts
@@ -1,10 +1,10 @@
-let json = require('./json-metrics')
-let metricsArray: Promise<any>[] = [];
+const json = require('./json-metrics')
+const metricsArray: Promise<any>[] = [];
 
 export default async function getMetricsFromURL(urls: Array<string>, downSpeed: number, upSpeed: number, lat: number,) {
     
     let device ='iPhone 8'
-    
+
     urls.forEach(async element => {
         metricsArray.push(json(element, downSpeed, upSpeed, lat));
     });
