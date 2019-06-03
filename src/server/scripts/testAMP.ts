9c4e206352349e1f8a7832d5718d4df5e765af8a
Anthony Odum <anthonyodum@google.com>
2019-06-03 10:17:28 -0700

    fixed import, added interface

--- src/server/scripts/testAMP.ts
+++ src/server/scripts/testAMP.ts
@@ -1,12 +1,18 @@
-const json = require('./json-metrics')
+import getMetricsFromURL from './json-metrics';
 const metricsArray: Promise<any>[] = [];
 
-export default async function getMetricsFromURL(urls: Array<string>, downSpeed: number, upSpeed: number, lat: number,) {
+interface PagePerf {
+    device: string;
+    networkSpeed: string;
+    metrics: string[];
+}
+
+export default async function getMetricsFromURLs(urls: Array<string>, downSpeed: number, upSpeed: number, lat: number): Promise<PagePerf> {
     
     let device ='iPhone 8'
 
     urls.forEach(async element => {
-        metricsArray.push(json(element, downSpeed, upSpeed, lat));
+        metricsArray.push(getMetricsFromURL(element, downSpeed, upSpeed, lat));
     });
 
     const result = await Promise.all(metricsArray);
