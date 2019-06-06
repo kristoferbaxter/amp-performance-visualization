d1d847bbe0d1bfe5ce76a824bd39cddba9305481
Anthony Odum <anthonyodum@google.com>
2019-06-06 11:06:44 -0700

    AMP Wgt metric, perf mark, AMP site tester added

--- src/server/scripts/json-metrics.ts
+++ src/server/scripts/json-metrics.ts
@@ -2,7 +2,8 @@
  * @fileoverview Description of this file.
  */
 const puppeteer = require('puppeteer');
-
+const fetch = require('node-fetch');
+const performance = require('perf_hooks')
 
 interface PagePerformance {
     url: string;
@@ -10,6 +11,14 @@ interface PagePerformance {
     pageLoad: number;
     interactive: number;
     firstContentfulPaint: number;
+    ampWeight: number;
+    installStyles: number;
+    endInstallStyles: number;
+    visible: number;
+    onFirstVisible: number;
+    makeBodyVisible: number;
+    windowLoadEvent: number;
+    firstViewportReady: number
   }
 
 //===============Metrics Methods===============
@@ -25,22 +34,24 @@ const getTimeToInteractive = (results:PerformanceTiming) => (results.domInteract
 //Time Until the First Contentful Paint
 const getTimeToFirstContentfulPaint = (results:PerformanceTiming) => (results.domLoading - results.navigationStart);
 
-//AMP Resource Weight
-/*let ampResourceWgt = async (results:PerformanceTiming) => {
-    let weight = 0;
-    const weightArray = performance.getEntriesByType('resource').filter(item => {
-        return (item as PerformanceResourceTiming).initiatorType === 'script' && item.name.startsWith('https://cdn.ampproject.org/')})
-    weightArray.forEach(element => {
-        weight += (element as PerformanceResourceTiming).transferSize
-    });
-    return weight
-}*/
 
 const getMetrics = async (webpage: string, downSpeed: number, upSpeed: number, lat: number):Promise<PagePerformance> => {
+    
+    if(!(await isAMP(webpage))) {
+        return {
+            url: webpage,
+            firstByte: -2,
+            pageLoad: -2,
+            interactive: -2,
+            firstContentfulPaint: -2,
+            ampWeight: -2
+        } as PagePerformance
+    }
+    
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     //Sets the navigation timeout to 2 minutes
-    await page.setDefaultNavigationTimeout(120000)
+    
 
     /*Emulating a Wifi connection
     "/8" is included because network speed is commonly measured in bits/s
@@ -56,22 +67,73 @@ const getMetrics = async (webpage: string, downSpeed: number, upSpeed: number, l
     //waits until the page is fully loaded
     try {
         await page.goto(webpage, {
+            timeout: 0,
             waitUntil: 'networkidle0'
         })
     } catch (e) {
-        //console.log(e);
+        // return {
+        //     url: webpage,
+        //     firstByte: -1,
+        //     pageLoad: -1,
+        //     interactive: -1,
+        //     firstContentfulPaint: -1,
+        //     ampWeight: -1
+        // } as PagePerformance
     }
+
+    
+
     //Returning info
     const results = JSON.parse(await page.evaluate(() => {
         return JSON.stringify(performance.timing);
      }))
 
+     //Returning AMP Resource Weight
+     
+    const ampWeight = JSON.parse(await page.evaluate(() => {
+        let weight = 0;
+        const weightArray = performance.getEntriesByType('resource').filter((item:PerformanceResourceTiming) => {
+            return item.initiatorType === 'script' && item.name.startsWith('https://cdn.ampproject.org/')
+        })
+        weightArray.forEach((element:PerformanceResourceTiming) => {
+            weight += element.transferSize
+        });
+        return JSON.stringify(weight)
+    }))
+
+    let marks:PerformanceEntryList;
+    const performanceMarks:number[] = JSON.parse(await page.evaluate(() => {
+        const markNames: string[] = ['is','e_is','visible','ofv','mbv','ol','pc'];
+        const markValues: number[] = [];
+        markNames.forEach((element) => {
+            marks = performance.getEntriesByType('mark').filter((item:PerformanceMark) => 
+                item.name.startsWith(element)
+            );
+            if(marks[0]){
+            let markTime = (Math.round(marks[0].startTime * 1000))/1000
+            markValues.push(markTime);
+        }
+        });
+
+        return JSON.stringify(markValues);
+    }))
+
+    await browser.close()
+    
     return {
-        url:page.url(),
+        url: webpage,
         firstByte: getTimeToFirstByte(results),
         pageLoad: getTimeToPageLoaded(results),
         interactive: getTimeToInteractive(results),
-        firstContentfulPaint: getTimeToFirstContentfulPaint(results)
+        firstContentfulPaint: getTimeToFirstContentfulPaint(results),
+        ampWeight: ampWeight,
+        installStyles: performanceMarks[0],
+        endInstallStyles: performanceMarks[1],
+        visible: performanceMarks[2],
+        onFirstVisible: performanceMarks[3],
+        makeBodyVisible: performanceMarks[4],
+        windowLoadEvent: performanceMarks[5],
+        firstViewportReady: performanceMarks[6]
     }
 }
 
@@ -83,14 +145,32 @@ const getResults = async (webpage: string, downSpeed: number, upSpeed: number, l
                 firstByte: -1,
                 pageLoad: -1,
                 interactive: -1,
-                firstContentfulPaint: -1
+                firstContentfulPaint: -1,
+                ampWeight: -1
             } as PagePerformance)
-        },100000)
+        },115000)
     })
 
     const pageMetrics = getMetrics(webpage, downSpeed, upSpeed, lat);
 
-    return await Promise.race([slowURL, pageMetrics]);
+    return await Promise.race([
+        slowURL,
+        pageMetrics
+    ]);
 }
  
 export default getResults
+
+const isAMP = async (url: string): Promise<boolean> => {
+    try {
+        const response = await fetch(url);
+        const bodyText = await response.text();
+        if (/<html\s[^>]*(⚡|amp)[^>]*>/.test(bodyText)) {
+            return true
+        }
+        return false;
+    } catch(e) {
+        return false
+    }
+}
+
