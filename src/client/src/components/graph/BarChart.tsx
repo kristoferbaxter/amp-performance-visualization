ec2bb24618e9cfe6b5016f743dadef8bb1b06da6
Jortiz23 <Jortiz23@verizon.net>
2019-06-11 11:37:02 -0700

    Consolidating functions and tidying up code

--- src/client/src/components/graph/BarChart.tsx
+++ src/client/src/components/graph/BarChart.tsx
@@ -1,97 +1,70 @@
 import { Component, h } from 'preact';
-import {Axis} from './Axis'
-import {Bar} from './Bar'
-import style from "./BarChart.css";
-import {Label} from './Label'
+import { Axis } from './Axis';
+import { Bar } from './Bar';
+import style from './BarChart.css';
+import { Label } from './Label';
 
-interface Props{
+interface Props {
   data: {
-    timeToFirstByte: number,
-    timeToFirstContentfulPaint: number,
-    timeToInteractive: number,
-    timeToPageLoad: number,
-    ampResourceWgt: number
-  }
+    timeToFirstByte: number;
+    timeToFirstContentfulPaint: number;
+    timeToInteractive: number;
+    timeToPageLoad: number;
+    ampResourceWgt: number;
+  };
   svgWidth: number;
-  svgHeight:number;
-  barOffset:number;
+  svgHeight: number;
+  barOffset: number;
 }
 
-function getMaxValue(props: Props):number {
-  const { data } = props;
-  const onlyMetrics: number[] = Object.values(data);
-  const maxValue = Math.max.apply(null, onlyMetrics);
-  return Math.ceil(maxValue/100)*100;// rounds the number up to the nearest hundred
-}
-function getNumOfMetrics(props: Props):number {
-  const { data } = props;
-  return Object.keys(data).length
-}
-function getSvgX(x: number, props: Props):number {
-  const { svgWidth } = props;
-  return (x / getNumOfMetrics(props) * svgWidth);
-}
-function getSvgY(y:number, props: Props):number {
-  const { svgHeight } = props;
-  return svgHeight - (y / getMaxValue(props) * svgHeight);
-}
+interface State {}
 
-class BarChart extends Component<Props, {}> {
+const maxValue = ({ data }: Props): number => Math.ceil(Math.max.apply(null, Object.values(data)));
+const numOfMetrics = ({ data }: Props): number => Object.keys(data).length;
+const svgX = (x: number, props: Props): number => (x / numOfMetrics(props)) * props.svgWidth;
+const svgY = (y: number, props: Props): number => props.svgHeight - (y / maxValue(props)) * props.svgHeight;
+
+class BarChart extends Component<Props, State> {
   public static defaultProps = {
     svgHeight: 800,
     svgWidth: 800,
     barOffset: 30,
-  }
+  };
 
-  public render() {
-    const { data, svgHeight, svgWidth, barOffset } = this.props;
+  public render({ data, svgHeight, svgWidth, barOffset }: Props): JSX.Element {
     const valueArr = Object.values(data);
     const keyArr = Object.keys(data);
     return (
-      <svg viewBox={`0 0 ${svgWidth} ${svgHeight}`} className={style.graph} width={svgWidth} height={svgHeight}>
-        <g className={style.barChartRects}>
-        {
-          valueArr.map((value, index) => (
+      <svg viewBox={`0 0 ${svgWidth} ${svgHeight}`} class={style.graph} width={svgWidth} height={svgHeight}>
+        <g class={style.barChartRects}>
+          {valueArr.map((value, index) => (
             <Bar
-              x={getSvgX(index, this.props)+barOffset}
-              y={getSvgY(value, this.props)}
-              width={(svgWidth/3)/getNumOfMetrics(this.props)}// the 2/3 scales the bars so they will always fit on the graph for "N" number of metrics
-              height={svgHeight-getSvgY(value, this.props)}
+              x={svgX(index, this.props) + barOffset}
+              y={svgY(value, this.props)}
+              width={svgWidth / 3 / numOfMetrics(this.props)} // the 2/3 scales the bars so they will always fit on the graph for "N" number of metrics
+              height={svgHeight - svgY(value, this.props)}
             />
-          ))
-        }
+          ))}
         </g>
         <Axis
-          minX={getSvgX(0, this.props)}
-          minY={getSvgY(0, this.props)}
-          maxX={getSvgX(getNumOfMetrics(this.props)+1, this.props)}// the +1 increases the length of the axis so it appears correctly
-          maxY={getSvgY(getMaxValue(this.props), this.props)}
+          minX={svgX(0, this.props)}
+          minY={svgY(0, this.props)}
+          maxX={svgX(numOfMetrics(this.props) + 1, this.props)} // the +1 increases the length of the axis so it appears correctly
+          maxY={svgY(maxValue(this.props), this.props)}
         />
         <g>
-        {
-          valueArr.map((value, index) => (
-            <Label
-              x={getSvgX(index, this.props)+barOffset}
-              y={getSvgY(value, this.props)}
-              value={value}
-            />
-          ))
-        }
+          {valueArr.map((value, index) => (
+            <Label x={svgX(index, this.props) + barOffset} y={svgY(value, this.props)} value={value} />
+          ))}
         </g>
         <g>
-        {
-          keyArr.map((value, index) => (
-            <Label
-              x={getSvgX(index, this.props)+barOffset}
-              y={svgHeight}
-              value={value}
-            />
-          ))
-        }
+          {keyArr.map((value, index) => (
+            <Label x={svgX(index, this.props) + barOffset} y={svgHeight} value={value} />
+          ))}
         </g>
       </svg>
-    )
+    );
   }
 }
 
-export default BarChart
+export default BarChart;
