bcfaffeccff09dd771d415029d35faabe534f736
Jortiz23 <Jortiz23@verizon.net>
2019-06-18 15:15:05 -0700

    Remove temporary comments

--- src/components/graph/Bar.tsx
+++ src/components/graph/Bar.tsx
@@ -8,16 +8,3 @@ interface Props {
 }
 
 export const Bar = ({ x, y, width, height }: Props): JSX.Element => <rect x={x} y={y} width={width} height={height} />;
-// export const ValueLabel = ({ x, y, value }: Props): JSX.Element => {
-//   let newY = y;
-//   let color = 'black';
-//   if (y < 17) {
-//     newY = y + 17;
-//     color = 'white';
-//   }
-//   return (
-//     <text fill={color} x={x} y={newY}>
-//       {value}
-//     </text>
-//   );
-// };
