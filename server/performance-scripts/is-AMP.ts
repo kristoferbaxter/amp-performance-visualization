96508a1dc9ad9e60c55e84099327a48cc79226d0
Anthony <anthony.odum14@gmail.com>
2019-06-18 13:13:59 -0700

    New Interface for to return information (#15)
    * Adding Puppeteer Script
    
    * Needs typeScript Review
    
    * forgot to commit config
    
    * renaming git ignore
    
    * old file
    
    * deleted old json-metrics file
    
    * fixed merge conflicts
    
    * new statistics interface
    
    * added temporary output
    
    * fixing merge conflicts
    
    * notAMP -> invalidAMP

--- server/performance-scripts/is-AMP.ts
+++ server/performance-scripts/is-AMP.ts
@@ -2,15 +2,15 @@ import fetch from 'node-fetch';
 
 const isAMP = async (url: string): Promise<boolean> => {
   try {
-      const response = await fetch(url);
-      const bodyText = await response.text();
-      if (/<html\s[^>]*(⚡|amp)[^>]*>/.test(bodyText)) {
-          return true
-      }
-      return false;
-  } catch(e) {
-      return false
+    const response = await fetch(url);
+    const bodyText = await response.text();
+    if (/<html\s[^>]*(⚡|amp)[^>]*>/.test(bodyText)) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return false;
   }
-}
+};
 
 export default isAMP;
--- server/performance-scripts/json-metrics.ts
+++ server/performance-scripts/json-metrics.ts
@@ -2,66 +2,47 @@
  * @fileoverview Description of this file.
  */
 import { launch } from 'puppeteer';
-import { getTimeToFirstByte, getTimeToPageLoaded } from './page-metrics-evaluation';
 import isAMP from './is-AMP';
+import { getTimeToFirstByte, getTimeToPageLoaded } from './page-metrics-evaluation';
+import Statistics, { failedPageGoTo, invalidAMP, snailURL } from './performance-data';
 
-export interface PagePerformance {
-  url: string;
-  firstByte: number;
-  pageLoad: number;
-}
-
-
-//const for line 58
+// Temporary output. Only to show new interface, not the actual output
+const TEMP_OUTPUT = 0;
+// const for line 58
 const NAV_TIMEOUT = 120000;
-//URL provided is not AMP
-
 // networkidle0 means that there are no more than 0 network connections for atleast 500 milliseconds
 const NAVIGATION_COMPLETE = 'networkidle0';
 
-// URL provided is not AMP
-const NOT_AMP = -2;
-// URL took too long to process.
-const SLOW_URL = -1;
+const getMetrics = async (url: string, downSpeed: number, upSpeed: number, latency: number): Promise<Statistics> => {
+  if (!(await isAMP(url))) {
+    return invalidAMP(url);
+  }
 
-
-const getMetrics = async (url: string, downSpeed: number, upSpeed: number, lat: number): Promise<PagePerformance> => {
-  
-  if(!(await isAMP(url))) {
-    return {
-        url,
-        firstByte: NOT_AMP,
-        pageLoad: NOT_AMP,
-    }
-}
   const browser = await launch();
   const page = await browser.newPage();
-  
+  // Sets the navigation timeout to 2 minutes
+  await page.setDefaultNavigationTimeout(NAV_TIMEOUT);
 
-  /*Emulating a Wifi connection
+  /*
+    Emulating a Wifi connection
     "/8" is included because network speed is commonly measured in bits/s
-    DevTools expects throughputs in bytes/s*/
+    DevTools expects throughputs in bytes/s
+  */
   const client = await page.target().createCDPSession();
   await client.send('Network.emulateNetworkConditions', {
     offline: false,
     downloadThroughput: (downSpeed * 1024) / 8,
     uploadThroughput: (upSpeed * 1024) / 8,
-    latency: lat,
+    latency,
   });
 
   // waits until the page is fully loaded
-  // TODO: handle navigationTimeouts
   try {
     await page.goto(url, {
-      timeout: 0, //disables navigation timeout
       waitUntil: NAVIGATION_COMPLETE,
     });
   } catch (e) {
-    return {
-      url,
-      firstByte: SLOW_URL,
-      pageLoad: SLOW_URL,
-    };
+    return failedPageGoTo(url);
   }
 
   // Returning info
@@ -73,28 +54,37 @@ const getMetrics = async (url: string, downSpeed: number, upSpeed: number, lat: 
 
   return {
     url,
-    firstByte: getTimeToFirstByte(results),
-    pageLoad: getTimeToPageLoaded(results),
+    responseStart: getTimeToFirstByte(results),
+    loadEventEnd: getTimeToPageLoaded(results),
+    domInteractive: TEMP_OUTPUT,
+    firstPaint: TEMP_OUTPUT,
+    firstContentfulPaint: TEMP_OUTPUT, // still working on adding these metrics
+    firstMeaningfulPaint: TEMP_OUTPUT,
+    custom: {
+      ampJavascriptSize: [],
+      installStyles: [TEMP_OUTPUT, TEMP_OUTPUT],
+      visible: TEMP_OUTPUT,
+      onFirstVisible: TEMP_OUTPUT,
+      makeBodyVisible: TEMP_OUTPUT,
+      windowLoadEvent: TEMP_OUTPUT,
+      firstViewportReady: TEMP_OUTPUT,
+    },
   };
 };
 
-const getResults = async (webpage: string, downSpeed: number, upSpeed: number, lat: number) => {
-  const slowURL = new Promise (resolve => {
-      setTimeout(() => {
-          resolve({
-              url: webpage,
-              firstByte: -1,
-              pageLoad: -1,
-          })
-      },NAV_TIMEOUT-100)
-  })
+const getResults = async (url: string, downSpeed: number, upSpeed: number, lat: number) => {
+  const slowURLReturn = snailURL(url);
+  const slowURL = new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        slowURLReturn,
+      });
+    }, NAV_TIMEOUT - 100);
+  });
 
-  const pageMetrics = getMetrics(webpage, downSpeed, upSpeed, lat);
+  const pageMetrics = getMetrics(url, downSpeed, upSpeed, lat);
 
-  return await Promise.race([
-      slowURL,
-      pageMetrics
-  ]);
-}
+  return await Promise.race([slowURL, pageMetrics]);
+};
 
 export default getResults;
