a5db42cf9780c2702fec30f2874bd9211eecc79b
Anthony Odum <anthonyodum@google.com>
2019-06-18 16:03:11 -0700

    new testing files

---
+++ server/performance-scripts/metrics-from-url-array.ts
@@ -0,0 +1,23 @@
+import getResults from './json-metrics';
+import Statistics from './performance-data';
+
+interface Results {
+  device: string;
+  networkSpeed: string;
+  metrics: Statistics[];
+}
+
+const DEVICE_NAME = 'iPhone 8';
+// haven't figured out how to get device info from user, so its hardcoded for now
+
+export default async function getMetricsFromURLs(urls: string[], downSpeed: number, upSpeed: number, latency: number): Promise<Results> {
+  const metricsArray: Array<Promise<Statistics>> = [];
+
+  urls.forEach(url => metricsArray.push(getResults(url, downSpeed, upSpeed, latency)));
+
+  return {
+    device: DEVICE_NAME,
+    networkSpeed: `downspeed: ${downSpeed}kbps`,
+    metrics: await Promise.all(metricsArray),
+  };
+}
