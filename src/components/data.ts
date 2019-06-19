5b080edeee3ea16d5e8baa3df3a726c7c0a665eb
Jortiz23 <Jortiz23@verizon.net>
2019-06-19 16:16:24 -0700

    Shifting interfaces to be in a shared folder

--- src/components/data.ts
+++ src/components/data.ts
@@ -1,30 +1,5 @@
+import { Metrics } from '../../shared-interfaces/metricsResults';
 import { results } from './results';
-
-interface Metrics {
-  url: string;
-  responseStart: number; // firstByte
-  loadEventEnd: number; // pageLoad
-  domInteractive: number; // interactive
-  firstPaint: number;
-  firstContentfulPaint: number; // use Performance.metrics injected into webpage
-  firstMeaningfulPaint: number;
-  custom: AMPCustomStatistics;
-}
-
-export interface AMPJavaScriptSizeEntry {
-  url: string;
-  size: number;
-}
-
-export interface AMPCustomStatistics {
-  ampJavascriptSize: AMPJavaScriptSizeEntry[];
-  installStyles: number[];
-  visible: number;
-  onFirstVisible: number;
-  makeBodyVisible: number;
-  windowLoadEvent: number;
-  firstViewportReady: number;
-}
 
 function getMedian(numArray: number[]): number {
   const sortedArr = numArray.sort((a, b) => a - b);
