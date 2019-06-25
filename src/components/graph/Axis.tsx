b9b990df83825061a3033d925431183475933a29
Jortiz23 <Jortiz23@verizon.net>
2019-06-25 10:49:50 -0700

    Separate css modules for each Component rather than having them all combined

--- src/components/graph/Axis.tsx
+++ src/components/graph/Axis.tsx
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-import style from './Chart.css';
+import style from './Axis.css';
 
 interface Props {
   minX: number;
--- src/components/graph/XLabel.tsx
+++ src/components/graph/XLabel.tsx
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-import style from './Chart.css';
+import style from './XLabel.css';
 
 interface Props {
   x: number;
