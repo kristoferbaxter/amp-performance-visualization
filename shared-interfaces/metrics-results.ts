2f1d5536623df315e60ffea10cfd664ab3fd6d6c
Anthony Odum <anthonyodum@google.com>
2019-06-26 11:12:00 -0700

    refactored JSON format

--- shared-interfaces/metrics-results.ts
+++ shared-interfaces/metrics-results.ts
@@ -1,27 +1,27 @@
 export interface Metrics {
-  url: string;
+  graphableData: NumberMetrics;
+  tableData: AMPJavaScriptSizeEntry[];
+}
+
+export interface NumberMetrics {
   responseStart: number; // firstByte
   loadEventEnd: number; // pageLoad
   domInteractive: number; // interactive
   firstPaint: number;
   firstContentfulPaint: number; // use Performance.metrics injected into webpage
   firstMeaningfulPaint: number;
-  custom: AMPCustomMetrics;
-}
-
-export interface AMPJavaScriptSizeEntry {
-  url: string;
-  size: number;
-}
-
-export interface AMPCustomMetrics {
-  ampJavascriptSize: AMPJavaScriptSizeEntry[];
-  installStyles: [number, number];
+  installStyles: number;
+  installStylesDuration: number;
   visible: number;
   onFirstVisible: number;
   makeBodyVisible: number;
   windowLoadEvent: number;
   firstViewportReady: number;
+}
+
+export interface AMPJavaScriptSizeEntry {
+  url: string;
+  size: number;
 }
 
 export interface MultipleRuns {
