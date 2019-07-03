c311155edb051739e4193b9437933ade5b494fb9
Anthony Odum <anthonyodum@google.com>
2019-07-03 13:37:54 -0700

    created an async function implementing a promisified fs.access and yargs

--- server/performance-scripts/test-run.ts
+++ server/performance-scripts/test-run.ts
@@ -4,36 +4,75 @@ import { NetworkJSON } from './json-metrics';
 import multiRunMetrics, { URLFileJSON } from './metrics-from-url-array';
 
 const readFile = promisify(fs.readFile);
+const checkFile = promisify(fs.access);
 const returnFileContent: (path: string) => Promise<string> = (location: string): Promise<string> => readFile(location, 'utf8');
+const checkIfFileReadable = (path: string): Promise<void | Error> => checkFile(path, fs.constants.R_OK);
+const argv = require('yargs').argv;
 
-// Metrics returning all -1 means the url took longer than 4 minutes to load
-// Metrics returning all -2 means the url is not AMP
-// Metrics returning all -3 means the program failed to go to the page
+const DEFAULT_NETWORK: NetworkJSON = {
+  downSpeed: 1536,
+  upSpeed: 750,
+  latency: 40,
+};
 
-Promise.all([returnFileContent(process.argv[2]), returnFileContent(process.argv[3])])
-  .then(([urlsFile, networkFile]) => {
-    try {
-      const parsedURLs: URLFileJSON = JSON.parse(urlsFile);
-      const parsedNetwork: NetworkJSON = JSON.parse(networkFile);
-      let numRuns: number = 3;
-      if (process.argv[4]) {
-        numRuns = Number.parseInt(process.argv[4]);
-      } else {
-        console.log('Number of Runs was not given, using default value of 3');
-      }
-
-      multiRunMetrics(parsedURLs, parsedNetwork, numRuns).then(data => {
-        fs.writeFile('server/results/amp-metrics.json', JSON.stringify(data, null, 2), err => {
-          if (err) {
-            throw err;
-          }
-          process.exit(0);
-        });
-      });
-    } catch (e) {
-      console.log('There was an exception running metrics', e);
+async function run() {
+  await checkIfFileReadable(argv.urls).then(err => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  })
-  .catch(e => {
-    console.log('there was an error finding your files', e);
+    return;
   });
+
+  let parsedNetwork: NetworkJSON;
+
+  await checkIfFileReadable(argv.network).then(err => {
+    if (err) {
+      parsedNetwork = DEFAULT_NETWORK;
+      console.log(
+        `Using default network settings: downSpeed ${parsedNetwork.downSpeed} kb/s | upSpeed ${parsedNetwork.upSpeed} kb/s | latency ${
+          parsedNetwork.latency
+        }`,
+      );
+      return;
+    }
+    returnFileContent(argv.network).then(networkFile => {
+      parsedNetwork = JSON.parse(networkFile);
+    });
+    return;
+  });
+
+  // Metrics returning all -1 means the url took longer than 4 minutes to load
+  // Metrics returning all -2 means the url is not AMP
+  // Metrics returning all -3 means the program failed to go to the page
+
+  returnFileContent(argv.urls)
+    .then(urlsFile => {
+      try {
+        const parsedURLs: URLFileJSON = JSON.parse(urlsFile);
+        let numRuns: number = 3;
+
+        if (argv.runs) {
+          numRuns = argv.runs;
+        } else {
+          console.log(`Number of Runs not specified, 3 Runs will be returned`);
+        }
+
+        multiRunMetrics(parsedURLs, parsedNetwork, numRuns).then(data => {
+          fs.writeFile('server/results/amp-metrics.json', JSON.stringify(data, null, 2), err => {
+            if (err) {
+              throw err;
+            }
+            process.exit(0);
+          });
+        });
+      } catch (e) {
+        console.log('There was an exception running metrics', e);
+      }
+    })
+    .catch(e => {
+      console.log('there was an error finding your files', e);
+    });
+}
+
+run();
