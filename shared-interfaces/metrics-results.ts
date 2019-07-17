99557f62f464c3fff1d6caf96e079a7c0e20fac9
Anthony <anthony.odum14@gmail.com>
2019-07-17 11:37:21 -0700

    Add files via upload

---
+++ shared-interfaces/metrics-results.ts
@@ -0,0 +1,32 @@
+export interface TestPass {
+  device: string;
+  networkSpeed: string;
+  results: Results[];
+}
+
+export interface Results {
+  url: string;
+  performance: TimeMetrics[];
+  amp: AMPEntry[];
+}
+
+export interface TimeMetrics {
+  responseStart: number; // firstByte
+  loadEventEnd: number; // pageLoad
+  interactive: number; // interactive
+  firstPaint: number;
+  firstContentfulPaint: number; // use Performance.metrics
+  firstMeaningfulPaint: number;
+  installStyles: number;
+  installStylesDuration: number;
+  visible: number;
+  onFirstVisible: number;
+  makeBodyVisible: number;
+  windowLoadEvent: number;
+  firstViewportReady: number;
+}
+
+export interface AMPEntry {
+  url: string;
+  size: number;
+}
